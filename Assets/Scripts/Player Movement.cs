66d75c9e747b02bb2edd69c2c8b1db9018d0a6bc
Lewis Lovette <lewislovette3@hotmail.co.uk>
2020-02-06 19:46:29 +0000

    Probuilder installed, tentative hole created
    created a tentative hole currently. Learning probuilder so random objects for test and a test hole currently made.

--- Assets/Scripts/Player Movement.cs
+++ Assets/Scripts/Player Movement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    public CharacterController playerController;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,8 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //get data and pass to move
+        //playerController.Move();
         
     }
 }
